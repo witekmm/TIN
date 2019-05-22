8ad006a2ebc321c5e3ccd4c8b4c84ce102a380db
witekmm <witkowskimm@o2.pl>
2019-05-22 08:33:31 +0200

    new pipes

---
+++ Server/Pipes/LogicPipe.cpp
@@ -0,0 +1,55 @@
+#include "LogicPipe.h"
+
+//dont know what to create
+LogicPipe::LogicPipe(){
+  this->isInputMessageSet=false;
+  this->isOutputMessageSet=false;
+}
+
+void LogicPipe::setInputMessage(Message::ClientMessage& message){
+  pthread_mutex_lock(&this->mutex);
+  if(this->isInputMessageSet==true){
+    pthread_cond_wait(&this->inputCond, &this->mutex);
+    pthread_mutex_lock(&this->mutex);
+  }
+  this->inputMessage=Message::ClientMessage();
+  this->inputMessage.CopyFrom(message);
+  this->isInputMessageSet=true;
+  pthread_mutex_unlock(&this->mutex);
+}
+
+void LogicPipe::setOutputMessage(Message::ClientMessage& message){
+  pthread_mutex_lock(&this->mutex);
+  if(this->isOutputMessageSet==true){
+     pthread_cond_wait(&this->outputCond, &this->mutex);
+     pthread_mutex_lock(&this->mutex);
+  }
+  this->OutputMessage=Message::ClientMessage();
+  this->OutputMessage.CopyFrom(message);
+  this->isOutputMessageSet=true;
+  pthread_mutex_unlock(&this->mutex);
+}
+
+Message::ClientMessage LogicPipe::getOutputMessage(){
+  return this->outputMessage;
+}
+
+Message::ClientMessage LogicPipe::getInputMessage(){
+  return this->inputMessage;
+}
+
+void LogicPipe::clearInputMessage(){
+  pthread_mutex_lock(&this->mutex);
+  this->isInputMessageSet=false;
+  this->inputMessage=nullptr;
+  pthread_cond_signal(&this->inputCond);
+  pthread_mutex_unlock(&this->mutex);
+}
+
+void LogicPipe::clearOutputMessage(){
+  pthread_mutex_lock(&this->mutex);
+  this->isOutputMessageSet=false;
+  this->outputMessage=nullptr;
+  pthread_cond_signal(&this->outputCond);
+  pthread_mutex_unlock(&this->mutex);
+}
---
+++ Server/Pipes/LogicPipe.h
@@ -0,0 +1,28 @@
+#ifndef LOGIC_PIPE_H
+#define LOGIC_PIPE_H
+
+#include "../../Messages/Message.pb.h"
+
+#include <pthread.h>
+
+class LogicPipe{
+  //buffer for message to handle
+  Message::ClientMessage inputMessage;
+  //buffer for message to send
+  Message::ClientMessage outputMessage;
+  pthread_mutex_t mutex;
+  bool isInputMessageSet;
+  pthread_cond_t inputCond;
+  bool isOutputMessageSet;
+  pthread_cond_t outputCond;
+public:
+  LogicPipe();
+  void setInputMessage(Message::ClientMessage& message);
+  Message::ClientMessage getInputMessage();
+  void clearInputMessage();
+  void setOutputMessage(Message::ClientMessage& message);
+  Message::ClientMessage getOutputMessage();
+  void clearOutputMessage();
+};
+
+#endif
