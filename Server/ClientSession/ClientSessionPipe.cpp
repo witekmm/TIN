a3b182da5f3e7ee63e7535b444d35e9fbe836b44
wkurek <w.kurek2@gmail.com>
2019-05-25 13:32:18 +0200

    readBytesSize()

--- Server/ClientSession/ClientSessionPipe.cpp
+++ Server/ClientSession/ClientSessionPipe.cpp
@@ -1,8 +1,24 @@
 #include <pthread.h>
 #include <vector>
 
+#include "NetLibs.h"
 #include "../../Messages/Message.pb.h"
+
 #include "ClientSessionPipe.h"
+
+using namespace std;
+
+ClientSessionPipe(int socket) {
+    socketNumber = socket;
+    numberOfBytesToRead = MESSAGE_SIZE_BYTES_NUMBER;
+    bytesMessageSizeRead = false;
+}
+
+void clearReadBytesVariables() {
+    numberOfBytesToRead = MESSAGE_SIZE_BYTES_NUMBER;
+    bytesMessageSizeRead = false;
+    readBytesBuffer.clear();
+}
 
 int ClientSessionPipe::getSocketNumber() {
     return socketNumber;
@@ -24,7 +40,47 @@ void addWriteMessage(Message::ClientMessage message) {
     writeMessagesBuffer.push_back(message);
 }
 
+int readBytesSize() {
+    char *tmp = new char[numberOfBytesToRead];
+
+    int bytesReceived = recv(this->socketNumber, &temp, 
+        numberOfBytesToRead, MSG_DONTWAIT);
+
+    if(bytesReceived == MESSAGE_SIZE_BYTES_NUMBER) {
+        bytesMessageSizeRead = true;
+        numberOfBytesToRead = atoi(temp);
+
+        delete [] tmp;
+        return 0;
+    } else if(bytesReceived > 0 && bytesReceived < MESSAGE_SIZE_BYTES_NUMBER) {
+        numberOfBytesToRead -= bytesReceived;
+
+        string tmpString(tmp, bytesReceived);
+        readBytesBuffer += tmpString;
+
+        if(numberOfBytesToRead == 0) {
+            numberOfBytesToRead = atoi(readBytesBuffer.c_str());
+            bytesMessageSizeRead = true;
+        }
+
+        delete [] tmp;
+        return 0;
+    } else {
+        delete [] tmp;
+        return -1;
+    }
+}
+
 int ClientSessionPipe::readBytes() {
+    if(!bytesMessageSizeRead) {
+        int result = readBytesSize();
+        return result;
+    } else {
+        
+    }
+
+
+
     //TODO: read bytes
     //TODO: when full message is received deserialize it and push to writeMessagesBuffer
 
--- Server/ClientSession/ClientSessionPipe.h
+++ Server/ClientSession/ClientSessionPipe.h
@@ -1,23 +1,31 @@
 #ifndef CLIENT_SESSION_PIPE_H
 #define CLIENT_SESSION_PIPE_H
 
+#define MESSAGE_SIZE_BYTES_NUMBER 4
+
 #include <pthread.h>
 #include <vector>
 
+#include "NetLibs.h"
 #include "../../Messages/Message.pb.h"
+
+using namespace std;
 
 class ClientSessionPipe {
     int socketNumber;
 
     string readBytesBuffer;
     int numberOfBytesToRead;
-    bool readBytesMessageSizeRead;
+    bool bytesMessageSizeRead;
 
     vector<Message::ClientMessage> writeMessagesBuffer;
 
+    void clearReadBytesVariables();
     void addWriteMessage(Message::ClientMessage);
 
 public:
+    ClientSessionPipe(int);
+
     int getSocketNumber();
 
     size_t getWriteMessagesCount();
