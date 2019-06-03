c6db6a864b72f32261764215d2079f15b16e0100
Mateusz <matk100.100@gmail.com>
2019-06-03 20:53:01 +0200

    login update

--- Server/Logic/MessageHandler.cpp
+++ Server/Logic/MessageHandler.cpp
@@ -11,13 +11,13 @@ void MessageHandler::LogicThreadLoop(){
     switch (res)
     {
       case -3:
-        DataBaseConnector::Reply::incorrectMessage(result.first.getLocalId() , "Login doesn't exist!");
+        DataBaseConnector::Reply::incorrectMessage(result.first.getLocalId() , "Login doesn't exist!", Message::ClientMessage::LOG_IN);
         break;
       case -2:
-        DataBaseConnector::Reply::incorrectMessage(result.first.getLocalId() , "Wrong password!");
+        DataBaseConnector::Reply::incorrectMessage(result.first.getLocalId() , "Wrong password!", Message::ClientMessage::LOG_IN);
         break;
       case -1:
-        DataBaseConnector::Reply::incorrectMessage(result.first.getLocalId() , "Login doesn't exist!");
+        DataBaseConnector::Reply::incorrectMessage(result.first.getLocalId() , "Login doesn't exist!", Message::ClientMessage::LOG_IN);
         break;
       case 0:
         break;
